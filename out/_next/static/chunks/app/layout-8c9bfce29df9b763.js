ff9d43047c6e0a3407f7aaf1a3f73b769cb4334f
yetsyy <carvalloyetsy@gmail.com>
2025-10-06 00:24:13 -0300

    Contenido estático

---
+++ out/_next/static/chunks/app/layout-8c9bfce29df9b763.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{4811:function(e,n,t){Promise.resolve().then(t.t.bind(t,2972,23)),Promise.resolve().then(t.t.bind(t,9974,23)),Promise.resolve().then(t.t.bind(t,4798,23)),Promise.resolve().then(t.t.bind(t,2778,23))},2778:function(){}},function(e){e.O(0,[919,669,461,672,971,117,744],function(){return e(e.s=4811)}),_N_E=e.O()}]);
